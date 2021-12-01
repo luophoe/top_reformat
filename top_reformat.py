c83d45e223ae2068ff0037bfbf94e2085746007e
Phoebe Luo <z65luo@uwaterloo.ca>
2021-12-01 16:06:50 +0800

    Update top_reformat.py

--- top_reformat.py
+++ top_reformat.py
@@ -92,4 +92,175 @@ def top_reformat(top_file, result_file):
         elif line.find("input") >= 0 and line.find(",") >= 0:
             norm_form = 0
             end_check = 0
-            # delete the input part of the
+            # delete the input part of the line
+            line = line[6:]
+            # get the separated names
+            split_list = line.split(",")
+            # get rid of the extra content
+            for i in rangw(len(split_list)):
+                split_list[i] = stripextra(split_list[i])
+                split_list[i] = stripsemicolon(split_list[i])
+                if split_list[i] != "":
+                    result_file.write("input           " + list[i] + ";\n")
+            # identify the type of the declaration
+            dec_type = 1
+            # if the end of the declaration is reached
+            if line.find(";") >= 0:
+                end_check = 1
+        # the inout declaration starts
+        elif line.find("inout") >= 0 and line.find(",") >= 0:
+            norm_form = 0
+            end_check = 0
+            # delete the input part of the line
+            line = line[6:]
+            # get the separated names
+            split_list = line.split(",")
+            # get rid of the extra content
+            for i in rangw(len(split_list)):
+                split_list[i] = stripextra(split_list[i])
+                split_list[i] = stripsemicolon(split_list[i])
+                if split_list[i] != "":
+                    result_file.write("inout           " + list[i] + ";\n")
+            # identify the type of the declaration
+            dec_type = 2
+            # if the end of the declaration is reached
+            if line.find(";") >= 0:
+                end_check = 1
+        # still in the declaration
+        elif end_check == 0:
+            norm_form = 0
+            # get the separated names
+            split_list = line.split(",")
+            # get rid of the extra content
+            for i in range(len(split_list)):
+                split_list[i] = stripextra(split_list[i])
+                split_list[i] = stripsemicolon(split_list[i])
+                if dec_type == 0:
+                    if split_list[i] != "":
+                        result_file.write("output           " + split_list[i] + ";\n")
+                elif dec_type == 1:
+                    if split_list[i] != "":
+                        result_file.write("input           " + split_list[i] + ";\n")
+                elif dec_type == 2:
+                    if split_list[i] != "":
+                        result_file.write("inout           " + split_list[i] + ";\n")
+            # if the end of the declaration is reached
+            if line.find(";") >= 0:
+                end_check = 1
+        # if it is the end of the declaration
+        elif end_check == 1:
+            end_check = -1
+            dec_type = -1
+
+        # 2. if the line has bit width, reformat by calculating the number of tabs and write into result file
+        if (line.find("input") >= 0 or line.find("inout") >= 0 or line.find("output") >= 0) and (line.find("[") >= 0):
+            norm_form = 0
+            # calculate the number of tabs needed for writing
+            tab_count = 16 - line.find("]")
+            tab_str = ""
+            for i in range(tab_count):
+                tab_str = tab_str + " "
+            # write into result file
+            result_file.write(line[:line.find("]")+1] + tab_str + stripextra(line[line.find("]")+1:]) + "\n")
+
+        # 3. if the lines are wrapped by specify and endspecify
+        if line.find("specify") >= 0 and com_check == 0:
+            norm_form = 0
+            # add comment characters in front of it and write into result file
+            com_check = 1
+            result_file.write("// " + line)
+        elif com_check == 1:
+            norm_form = 0
+            # add comment characters in front of it and write intp result file
+            result_file.write("// " + line)
+        if line.find("endspecify") >= 0:
+            com_check = 0
+
+        # 4. if the lines are for instantiation
+        # the first comment line to indicate the start of instantiation
+        if line.find("inst") >= 0 and line.find("//") >= 0:
+            norm_form = 0
+            inst_check = 1
+            # find the instantiation file and find the module signal names
+            inst_file = stripextra(line[line.find("inst")+4:])
+            inst_file = open(inst_file, "r")
+            inst_file_check = 0
+            for line in inst_file:
+                if line.find("module") >= 0 and line.find("(") >= 0:
+                    inst_file_check = 0
+                    for line in inst_file:
+                        if line.find("module") >= 0 and line.find(")") >= 0:
+                            inst_file_check = 1
+                            line = line[line.find("(")+1:]
+                            inst_list_lower = line.split(",")
+                            for element in inst_list_lower:
+                                element = stripextra(element)
+                                element = stripsemicolon(element)
+                                if element != "" and element != "\n":
+                                    inst_list.append(element)
+                        elif inst_file_check == 1:
+                            inst_list_lower = line.split(",")
+                            for element in inst_list_lower:
+                                element = stripextra(element)
+                                element = stripsemicolon(element)
+                                if element != "" and element != "\n":
+                                    inst_list.append(element)
+                        if inst_file_check == 1 and line.find(")") >= 0:
+                            inst_file_check = 0
+        # find the moudle name and the port list
+        elif inst_check == 1:
+            norm_form = 0
+            if line.find("(") >= 0:
+                module_name = line[:line.find("(")]
+                result_file.write(module_name + " (\n")
+                line = line[line.find("(")+1:]
+                if line.find("{") >= 0 and line.find("}") >= 0:
+                    special_character = stripextra(line[line.find("{"):line.find("}")+1])
+                elif line.find("{") >= 0 and line.find("}") < 0:
+                    special_character = stripextra(line[line.find("{"):])
+                    special_character_check = 1
+                elif special_character_check == 1 and line.find("}") >= 0:
+                    special_character = special_character + stripextra(line[:line.find("}")+1])
+                inst_top_list = line.split(",")
+                for element in inst_top_list:
+                    element = stripextra(element)
+                    element = stripsemicolon(element)
+                    if element.find("{") >= 0:
+                        is_char = 1
+                    elif element.find("}") >= 0:
+                        is_char = 0
+                        tab_1 = ""
+                        for i in range(25-len(inst_list[inst_list_num])):
+                            tab_1 = tab_1 + " "
+                        tab_2 = ""
+                        for i in range(33-len(special_character)):
+                            tab_2 = tab_2 + " "
+                        result_file.write("         ." + inst_list[inst_list_num] + tab_1 + "(      " + special_character + tab_2 + "),\n")
+                        inst_list_num += 1
+
+                    elif element != "" and element != "\n" and is_char == 0:
+                        tab_1 = ""
+                        for i in range(25-len(inst_list[inst_list_nu,])):
+                            tab_1 = tab_1 + " "
+                        tab_2 = ""
+                        for i in range(33-len(element)):
+                            tab_2 = tab_2 + " "
+                        result_file.write("         ." + inst_list[inst_list_num] + tab_1 + "(      " + element + tab_2 + "),\n")
+                        inst_list_num += 1
+                if line.find(")") >= 0:
+                    result_file.write(");\n\n")
+                    inst_check = 0
+                    inst_list_num = 0
+                    del inst_list[-len(inst_list):]
+
+            # 5. if the line is not a part of the special lines, write directly into result_file
+            if norm_form == 1:
+                result_file.write(line)
+
+
+# ----------------------------------------------------Part 2. Main------------------------------------------------------
+# call top_file
+# top_reformat(top_file, result_file)
+
+top_reformat(top_file, result_file)
+print("result file " + result_file + " updated.")
